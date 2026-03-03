f857c1a131d4d9beaed1e8e6e904c2964a357d46
FloSett <florian.settler@t-online.de>
2026-03-03 16:32:43 +0100

    Delete hangman.py

--- hangman.py
+++
@@ -1,8 +0,0 @@
-# ============================================================
-#                    HANGMAN - Spielstart
-# ============================================================
-
-from hangman_logik import main                                                                    # Hauptfunktion aus der Spiellogik importieren
-
-# ── Programmstart ─────────────────────────────────────────────
-main()                                                                                            # Spiel starten
