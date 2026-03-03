544523c880312bd2adb35455342cdbdbf9d4a139
FloSett <florian.settler@t-online.de>
2026-03-03 14:10:39 +0100

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,8 @@
+# ============================================================
+#                    HANGMAN - Spielstart
+# ============================================================
+
+from hangman_logik import main                                                                    # Hauptfunktion aus der Spiellogik importieren
+
+# ── Programmstart ─────────────────────────────────────────────
+main()                                                                                            # Spiel starten
